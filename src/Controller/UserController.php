06fedd11788ceedf9a140e4aeef6157ffa31ab85
Abraham-jean-louis <malcomstille@gmail.com>
2021-02-22 19:56:26 +0100

    edit user done

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use App\Entity\Users;
 use App\Form\UserEntityType;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Orm\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -53,5 +54,28 @@ class UserController extends AbstractController
         $users = $this->getDoctrine()->getRepository(Users::class)->findAll();
 
         return $this->json($users);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit", methods={"PUT"})
+     */
+    public function Edit(Request $request, $id)
+    {
+        $entity = $this->getDoctrine()->getManager();
+
+        $edit = $entity->getRepository(Users::class)->find($id);
+        $data = $request->request->all();
+        
+        $form = $this->createForm(UserEntityType::class, $edit);
+        $form->submit($data);
+
+        $edit->setCreateAt(new \DateTime());
+
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($edit);
+        $em->flush();
+        
+        return $this->json($edit);
     }
 }
